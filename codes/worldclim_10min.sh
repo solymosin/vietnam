cc74bdc35a2184b67dd6eec8b0fb8115787934ce
solymosin <solymosi.norbert@gmail.com>
2024-07-31 21:32:10 +0200

    WP6

---
+++ codes/worldclim_10min.sh
@@ -0,0 +1,149 @@
+# https://www.worldclim.org/data/cmip6/cmip6_clim10m.html#google_vignette
+
+wget https://geodata.ucdavis.edu/cmip6/10m/ACCESS-CM2/ssp126/wc2.1_10m_bioc_ACCESS-CM2_ssp126_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/BCC-CSM2-MR/ssp126/wc2.1_10m_bioc_BCC-CSM2-MR_ssp126_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/CMCC-ESM2/ssp126/wc2.1_10m_bioc_CMCC-ESM2_ssp126_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/EC-Earth3-Veg/ssp126/wc2.1_10m_bioc_EC-Earth3-Veg_ssp126_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/GISS-E2-1-G/ssp126/wc2.1_10m_bioc_GISS-E2-1-G_ssp126_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/INM-CM5-0/ssp126/wc2.1_10m_bioc_INM-CM5-0_ssp126_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/IPSL-CM6A-LR/ssp126/wc2.1_10m_bioc_IPSL-CM6A-LR_ssp126_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MIROC6/ssp126/wc2.1_10m_bioc_MIROC6_ssp126_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MPI-ESM1-2-HR/ssp126/wc2.1_10m_bioc_MPI-ESM1-2-HR_ssp126_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MRI-ESM2-0/ssp126/wc2.1_10m_bioc_MRI-ESM2-0_ssp126_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/UKESM1-0-LL/ssp126/wc2.1_10m_bioc_UKESM1-0-LL_ssp126_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/ACCESS-CM2/ssp245/wc2.1_10m_bioc_ACCESS-CM2_ssp245_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/BCC-CSM2-MR/ssp245/wc2.1_10m_bioc_BCC-CSM2-MR_ssp245_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/CMCC-ESM2/ssp245/wc2.1_10m_bioc_CMCC-ESM2_ssp245_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/EC-Earth3-Veg/ssp245/wc2.1_10m_bioc_EC-Earth3-Veg_ssp245_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/GISS-E2-1-G/ssp245/wc2.1_10m_bioc_GISS-E2-1-G_ssp245_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/INM-CM5-0/ssp245/wc2.1_10m_bioc_INM-CM5-0_ssp245_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/IPSL-CM6A-LR/ssp245/wc2.1_10m_bioc_IPSL-CM6A-LR_ssp245_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MIROC6/ssp245/wc2.1_10m_bioc_MIROC6_ssp245_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MPI-ESM1-2-HR/ssp245/wc2.1_10m_bioc_MPI-ESM1-2-HR_ssp245_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MRI-ESM2-0/ssp245/wc2.1_10m_bioc_MRI-ESM2-0_ssp245_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/UKESM1-0-LL/ssp245/wc2.1_10m_bioc_UKESM1-0-LL_ssp245_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/ACCESS-CM2/ssp370/wc2.1_10m_bioc_ACCESS-CM2_ssp370_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/BCC-CSM2-MR/ssp370/wc2.1_10m_bioc_BCC-CSM2-MR_ssp370_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/CMCC-ESM2/ssp370/wc2.1_10m_bioc_CMCC-ESM2_ssp370_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/EC-Earth3-Veg/ssp370/wc2.1_10m_bioc_EC-Earth3-Veg_ssp370_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/GISS-E2-1-G/ssp370/wc2.1_10m_bioc_GISS-E2-1-G_ssp370_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/INM-CM5-0/ssp370/wc2.1_10m_bioc_INM-CM5-0_ssp370_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/IPSL-CM6A-LR/ssp370/wc2.1_10m_bioc_IPSL-CM6A-LR_ssp370_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MIROC6/ssp370/wc2.1_10m_bioc_MIROC6_ssp370_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MPI-ESM1-2-HR/ssp370/wc2.1_10m_bioc_MPI-ESM1-2-HR_ssp370_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MRI-ESM2-0/ssp370/wc2.1_10m_bioc_MRI-ESM2-0_ssp370_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/UKESM1-0-LL/ssp370/wc2.1_10m_bioc_UKESM1-0-LL_ssp370_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/ACCESS-CM2/ssp585/wc2.1_10m_bioc_ACCESS-CM2_ssp585_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/BCC-CSM2-MR/ssp585/wc2.1_10m_bioc_BCC-CSM2-MR_ssp585_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/CMCC-ESM2/ssp585/wc2.1_10m_bioc_CMCC-ESM2_ssp585_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/EC-Earth3-Veg/ssp585/wc2.1_10m_bioc_EC-Earth3-Veg_ssp585_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/GISS-E2-1-G/ssp585/wc2.1_10m_bioc_GISS-E2-1-G_ssp585_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/INM-CM5-0/ssp585/wc2.1_10m_bioc_INM-CM5-0_ssp585_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/IPSL-CM6A-LR/ssp585/wc2.1_10m_bioc_IPSL-CM6A-LR_ssp585_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MIROC6/ssp585/wc2.1_10m_bioc_MIROC6_ssp585_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MPI-ESM1-2-HR/ssp585/wc2.1_10m_bioc_MPI-ESM1-2-HR_ssp585_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MRI-ESM2-0/ssp585/wc2.1_10m_bioc_MRI-ESM2-0_ssp585_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/UKESM1-0-LL/ssp585/wc2.1_10m_bioc_UKESM1-0-LL_ssp585_2041-2060.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/ACCESS-CM2/ssp126/wc2.1_10m_bioc_ACCESS-CM2_ssp126_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/BCC-CSM2-MR/ssp126/wc2.1_10m_bioc_BCC-CSM2-MR_ssp126_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/CMCC-ESM2/ssp126/wc2.1_10m_bioc_CMCC-ESM2_ssp126_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/EC-Earth3-Veg/ssp126/wc2.1_10m_bioc_EC-Earth3-Veg_ssp126_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/GISS-E2-1-G/ssp126/wc2.1_10m_bioc_GISS-E2-1-G_ssp126_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/INM-CM5-0/ssp126/wc2.1_10m_bioc_INM-CM5-0_ssp126_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/IPSL-CM6A-LR/ssp126/wc2.1_10m_bioc_IPSL-CM6A-LR_ssp126_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MIROC6/ssp126/wc2.1_10m_bioc_MIROC6_ssp126_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MPI-ESM1-2-HR/ssp126/wc2.1_10m_bioc_MPI-ESM1-2-HR_ssp126_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MRI-ESM2-0/ssp126/wc2.1_10m_bioc_MRI-ESM2-0_ssp126_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/UKESM1-0-LL/ssp126/wc2.1_10m_bioc_UKESM1-0-LL_ssp126_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/ACCESS-CM2/ssp245/wc2.1_10m_bioc_ACCESS-CM2_ssp245_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/BCC-CSM2-MR/ssp245/wc2.1_10m_bioc_BCC-CSM2-MR_ssp245_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/CMCC-ESM2/ssp245/wc2.1_10m_bioc_CMCC-ESM2_ssp245_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/EC-Earth3-Veg/ssp245/wc2.1_10m_bioc_EC-Earth3-Veg_ssp245_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/GISS-E2-1-G/ssp245/wc2.1_10m_bioc_GISS-E2-1-G_ssp245_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/INM-CM5-0/ssp245/wc2.1_10m_bioc_INM-CM5-0_ssp245_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/IPSL-CM6A-LR/ssp245/wc2.1_10m_bioc_IPSL-CM6A-LR_ssp245_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MIROC6/ssp245/wc2.1_10m_bioc_MIROC6_ssp245_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MPI-ESM1-2-HR/ssp245/wc2.1_10m_bioc_MPI-ESM1-2-HR_ssp245_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MRI-ESM2-0/ssp245/wc2.1_10m_bioc_MRI-ESM2-0_ssp245_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/UKESM1-0-LL/ssp245/wc2.1_10m_bioc_UKESM1-0-LL_ssp245_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/ACCESS-CM2/ssp370/wc2.1_10m_bioc_ACCESS-CM2_ssp370_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/BCC-CSM2-MR/ssp370/wc2.1_10m_bioc_BCC-CSM2-MR_ssp370_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/CMCC-ESM2/ssp370/wc2.1_10m_bioc_CMCC-ESM2_ssp370_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/EC-Earth3-Veg/ssp370/wc2.1_10m_bioc_EC-Earth3-Veg_ssp370_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/GISS-E2-1-G/ssp370/wc2.1_10m_bioc_GISS-E2-1-G_ssp370_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/INM-CM5-0/ssp370/wc2.1_10m_bioc_INM-CM5-0_ssp370_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/IPSL-CM6A-LR/ssp370/wc2.1_10m_bioc_IPSL-CM6A-LR_ssp370_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MIROC6/ssp370/wc2.1_10m_bioc_MIROC6_ssp370_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MPI-ESM1-2-HR/ssp370/wc2.1_10m_bioc_MPI-ESM1-2-HR_ssp370_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MRI-ESM2-0/ssp370/wc2.1_10m_bioc_MRI-ESM2-0_ssp370_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/UKESM1-0-LL/ssp370/wc2.1_10m_bioc_UKESM1-0-LL_ssp370_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/ACCESS-CM2/ssp585/wc2.1_10m_bioc_ACCESS-CM2_ssp585_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/BCC-CSM2-MR/ssp585/wc2.1_10m_bioc_BCC-CSM2-MR_ssp585_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/CMCC-ESM2/ssp585/wc2.1_10m_bioc_CMCC-ESM2_ssp585_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/EC-Earth3-Veg/ssp585/wc2.1_10m_bioc_EC-Earth3-Veg_ssp585_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/GISS-E2-1-G/ssp585/wc2.1_10m_bioc_GISS-E2-1-G_ssp585_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/INM-CM5-0/ssp585/wc2.1_10m_bioc_INM-CM5-0_ssp585_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/IPSL-CM6A-LR/ssp585/wc2.1_10m_bioc_IPSL-CM6A-LR_ssp585_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MIROC6/ssp585/wc2.1_10m_bioc_MIROC6_ssp585_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MPI-ESM1-2-HR/ssp585/wc2.1_10m_bioc_MPI-ESM1-2-HR_ssp585_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/MRI-ESM2-0/ssp585/wc2.1_10m_bioc_MRI-ESM2-0_ssp585_2061-2080.tif
+wget https://geodata.ucdavis.edu/cmip6/10m/UKESM1-0-LL/ssp585/wc2.1_10m_bioc_UKESM1-0-LL_ssp585_2061-2080.tif
+
+# R
+
+library(raster)
+library(tidyverse)
+library(sf)
+
+# f='wc2.1_10m_bioc_UKESM1-0-LL_ssp585_2041-2060.tif'
+# shp = read_sf('/home/sn/dev/aote/Para/Vietnam/vietnam_github/maps/gadm41_VNM_1.shp')
+
+shp = read_sf('gadm41_VNM_1.shp')
+fs = list.files(pattern='.tif')
+for(f in fs){
+  tab = rasterToPoints(crop(raster(f, band=1), shp)) |> as_tibble() |> rename(val=3) |> mutate(band=1)
+  for(i in 2:19){
+    tab = bind_rows(tab, rasterToPoints(crop(raster(f, band=i), shp)) |> as_tibble() |> rename(val=3) |> mutate(band=i))
+  }
+  tab |> write_tsv(gsub('.tif', '.tsv', f))
+}
+
+
+shp = read_sf('/home/sn/dev/aote/Para/Vietnam/vietnam_github/maps/gadm41_VNM_1.shp')
+
+f = 'wc2.1_10m_bioc_UKESM1-0-LL_ssp585_2061-2080.tsv'
+fs = list.files(pattern='.tsv')
+for(f in fs){
+pts = read_tsv(f) |> st_as_sf(coords=c('x','y'), crs=4326)
+spts = pts |> filter(band==1)
+m = st_within(spts, shp, sparse=F)
+lst = apply(m, 2, which)
+stat = inner_join(tibble(gid=shp |> pull(GID_1), id=lst) |> unnest(cols = c(id)),
+spts |> select(val) |> st_drop_geometry() %>% mutate(id=1:dim(.)[1])
+) |> group_by(gid) |> summarize(mean=mean(val)) |> mutate(band=1)
+for(i in 2:19){
+  spts = pts |> filter(band==i)
+  m = st_within(spts, shp, sparse=F)
+  lst = apply(m, 2, which)
+  stat = bind_rows(stat, inner_join(tibble(gid=shp |> pull(GID_1), id=lst) |> unnest(cols = c(id)),
+  spts |> select(val) |> st_drop_geometry() %>% mutate(id=1:dim(.)[1])
+  ) |> group_by(gid) |> summarize(mean=mean(val)) |> mutate(band=i))
+}
+stat |> write_tsv(gsub('.tsv', '.stat', f))
+}
+
+# gcms = strsplit(gsub('.tsv', '', gsub('wc2.1_10m_bioc_', '', f)), '_')[[1]]
+# inner_join(
+# shp |> select(GID_1, NAME_1, VARNAME_1) |> rename(gid=1),
+# stat |> mutate(band=paste0('bio', formatC(band, width=2, flag='0'))) |> pivot_wider(names_from=band, values_from=mean) |> mutate(gcm=gcms[1], ssp=gsub('ssp', '', gcms[2]), period=gcms[3])
+# )
+
+fs = list.files(pattern='.stat')
+for(f in fs){
+stat = read_tsv(f)
+inner_join(
+shp |> select(GID_1, NAME_1, VARNAME_1) |> rename(gid=1),
+stat |> mutate(band=paste0('bio', formatC(band, width=2, flag='0'))) |> pivot_wider(names_from=band, values_from=mean)
+) |> st_write(driver='ESRI Shapefile', dsn=gsub('.stat', '', f), layer_options='ENCODING=UTF-8', delete_layer=T)
+}
+
